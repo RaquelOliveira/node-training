cd132058e71d43287c0393c66659a42bbec3e236
RaquelOliveira <ods.raquel@gmail.com>
2019-01-08 03:31:14 -0300

    implementando o controller

--- src/app.js
+++ src/app.js
@@ -6,35 +6,15 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+
+// Carregar as Rotas//////
+const indexRoute = require('./routers/index-route');
+const productRoute = require('./routers/products-route');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-})
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
