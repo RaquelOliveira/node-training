4d8ca2bdeec1bccd71e777db5b5737b5c2cdbed9
RaquelOliveira <ods.raquel@gmail.com>
2019-01-09 03:00:12 -0300

    listando todos os produtos

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -3,6 +3,18 @@
 const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
+exports.get = (req, res, next) => {
+    Product
+        .find({
+            active: true
+        }, 'title price slug')
+        .then( data => {
+            res.status(200).send(data);
+        }).catch(e => {
+            res.status(400).send(e);
+        });
+}
+
 exports.post = (req, res, next) => {
     const product = new Product(req.body);
     product
