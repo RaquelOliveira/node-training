f90b7ce1fca2ccd34a56a3374ee7e42feb5ca9dc
RaquelOliveira <ods.raquel@gmail.com>
2019-01-14 15:47:51 -0300

    atualizando e removendo produto

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -63,13 +63,37 @@ exports.post = (req, res, next) => {
 }
 
 exports.put = (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
+    Product
+    .findByIdAndUpdate(req.params.id, {
+        $set: {
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            slug: req.body.slug
+        }
+    }).then(x => {
+        res.status(201).send({
+            message: 'Produto atualizado com sucesso'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao atualizar produto',
+            data: e
+        });
     });
 };
 
 exports.delete = (req, res, next) =>{
-    res.status(200).send(req.body);
+    Product
+    .findOneAndDelete(req.body.id)
+    .then(x => {
+        res.status(200).send({
+            message: 'Produto removido com sucesso'
+        });
+    }).catch(e =>{
+        res.status(400).send({
+            message: 'Erro ao remover produto',
+            data: e
+        });
+    });
 };
